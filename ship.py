1cf8a6f9d7ac551f26502c9aba50fb1dd2c29632
Danil Osipov <danko11rus@gmail.com>
2020-03-23 10:44:53 +0300

    feat: task 12-1, 12-2

--- ship.py
+++ ship.py
@@ -12,7 +12,7 @@ class Ship():
         self.screen_rect = screen.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centery = self.screen_rect.centery
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
