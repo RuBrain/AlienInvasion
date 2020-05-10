7271dc52f7a31e6062460edd857e4045632948dc
Danil Osipov <danko11rus@gmail.com>
2020-05-10 17:41:59 +0300

    feat: bullet add

--- game_functions.py
+++ game_functions.py
@@ -1,8 +1,9 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Реагирует на нажатие клавиш."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -14,7 +15,12 @@ def check_keydown_events(event, ship):
         ship.moving_up = True
 
     elif event.key == pygame.K_DOWN:
-        ship.moving_down = True 
+        ship.moving_down = True
+
+    elif event.key == pygame.K_SPACE:
+        # Создание новой пули и включение ее в группу bullets.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet) 
 
 def check_keyup_events(event, ship):
     """Реагирует на отпускание клавиш."""
@@ -31,7 +37,7 @@ def check_keyup_events(event, ship):
         ship.moving_down = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Обрабатывает нажатия клавиш и события мыши."""
     for event in pygame.event.get():
 
@@ -39,15 +45,23 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Обновляет изображения на экране и отображает новый экран."""
+
+    # Все пули выводятся позади изображений корабля и пришельцев.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+        ship.blitme()
+
     # При каждом проходе цикла перерисовывается экран.
     screen.fill(ai_settings.bg_color)
     ship.blitme()
+
     # Отображение последнего прорисованного экрана.
     pygame.display.flip()
+   
--- settings.py
+++ settings.py
@@ -7,4 +7,9 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 140, 140)
-        self.ship_speed_factor = 0.9
+        self.ship_speed_factor = 1
+        # параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 30
