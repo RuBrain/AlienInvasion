574f2fc84820205e422427c610a57b781461a048
Danil Osipov <danko11rus@gmail.com>
2020-07-24 20:06:37 +0300

    feat: increase of cost

--- settings.py
+++ settings.py
@@ -22,10 +22,12 @@ class Settings():
         self.fleet_drop_speed = 15
 
         # Темп ускорения игры
-        self.speedup_scale = 1.4
-        
-        self.initialize_dynamic_settings()
+        self.speedup_scale = 1.1
 
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+        
     def initialize_dynamic_settings(self):
         """Инициализирует настройки, изменяющиеся в ходе игры."""
         self.ship_speed_factor = 8
@@ -39,7 +41,8 @@ class Settings():
         self.alien_points = 50
 
     def increase_speed(self):
-        """Увеличивает настройки скорости."""
+        """Увеличивает настройки скорости и стоимость пришельцев."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
