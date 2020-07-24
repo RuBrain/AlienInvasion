7aef04b5a494c6ac5cd6326797083778c4a2894a
Danil Osipov <danko11rus@gmail.com>
2020-07-24 20:48:21 +0300

    feat: record add

--- game_stats.py
+++ game_stats.py
@@ -9,6 +9,9 @@ class GameStats():
         # Игра Alien Invasion запускается в неактивном состоянии.
         self.game_active = False
 
+        # Рекорд не должен сбрасываться.
+        self.high_score = 0
+
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры."""
         self.ships_left = self.ai_settings.ship_limit
